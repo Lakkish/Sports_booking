75dcefa123040e7bdc5c1e811c435ba96a85d582
Lakkish <d.lashore2@gmail.com>
2025-12-10 19:49:29 +0530

    Added Home component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import Home from "./pages/Home.jsx";
 import BookingPage from "./pages/BookingPage";
 import BookingHistory from "./pages/BookingHistory";
 import AdminDashboard from "./pages/AdminDashboard";
